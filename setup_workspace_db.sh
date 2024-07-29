05df03d90b3b6bc230759a86a0c5fc0a4cac87af
Yakshit Jain <jainyakshit1@gmail.com>
2024-07-29 12:37:30 +0530

    updated changes - 2024-07-29 12:37:30

--- setup_workspace_db.sh
+++ setup_workspace_db.sh
@@ -13,5 +13,5 @@ echo "Initializing MySQL database..."
 sudo mysql -uroot -e "CREATE USER '$DB_USER'@'localhost' IDENTIFIED BY '$DB_PASS';"
 sudo mysql -uroot -e "GRANT ALL PRIVILEGES ON *.* TO '$DB_USER'@'localhost' WITH GRANT OPTION;"
 sudo mysql -uroot -e "FLUSH PRIVILEGES;"
-sudo mysql -u $DB_USER -e "DROP DATABASE IF EXISTS $DB_NAME; CREATE DATABASE $DB_NAME;"
-sudo mysql -u $DB_USER $DB_NAME < $DB_FILE
+sudo mysql -u $DB_USER -p$DB_PASS -e "DROP DATABASE IF EXISTS $DB_NAME; CREATE DATABASE $DB_NAME;"
+sudo mysql -u $DB_USER -p$DB_PASS $DB_NAME < $DB_FILE
